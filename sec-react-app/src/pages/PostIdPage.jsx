5e0f3866ae317e6b1c089ea61447c32cb12361a2
Викторов Павел <decidedtosolvethisproblem@gmail.com>
2025-04-30 17:39:15 +0300

    Added function for comments fetching

--- sec-react-app/src/pages/PostIdPage.jsx
+++ sec-react-app/src/pages/PostIdPage.jsx
@@ -6,21 +6,41 @@ import MyLoader from "../components/UI/loader/MyLoader.jsx";
 const PostIdPage = () => {
     const params = useParams();
     const [post, setPost] = useState({});
+    const [comments, setComments] = useState([]);
     const [fetchPostById, isLoading, error] = useFetching(async (id)=>{
         const response = await PostService.getById(id);
         console.log(response.data);
         setPost(response.data);
     })
+    const [fetchComments, isComLoading, comError] = useFetching(async (id)=>{
+        const response = await PostService.getCommentsByPostId(id);
+        console.log(response.data);
+        setComments(response.data);
+    })
     useEffect(() => {
         fetchPostById(params.id)
+        fetchComments(params.id)
     },[])
     return (
         <div>
             <h1>Вы открыли страницу поста с ID={params.id}!</h1>
-            {isLoading ?
-                <MyLoader/>
-                :
-                <div>{post.id},{post.title}</div>
+            {   isLoading ?
+                    <MyLoader/>
+                    :
+                    <div>{post.id},{post.title}</div>
+            }
+            <h2>Комментарии:</h2>
+            {isComLoading ?
+                    <MyLoader/>
+                    :
+                    <div>
+                        {comments.map(comment=>
+                            <div>
+                                <h3>{comment.email}</h3>
+                                <p>{comment.body}</p>
+                            </div>
+                        )}
+                    </div>
             }
         </div>
     );
